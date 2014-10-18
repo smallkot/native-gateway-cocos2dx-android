04ed43f64ce9498f2e51f892208f9d1fc8360774
vedi <shubin.fedor@gmail.com>
2014-10-18 16:40:42 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Native Gateway Cocos2dx Android
+---
+
+It's Cocos2dx Android adaptor of Native Gateway.
+
+Native Gateway
+---
+
+Native Gateway allows you to create flexible native extension for cross-platform engines in easy way.
+
+See implemetation of flurry integration as an example: https://github.com/vedi/flurry-bunch
+
---
+++ src/main/java/com/vedidev/nativebridge/cocos2dx/Cocos2dxGatewayAdapter.java
@@ -0,0 +1,37 @@
+package com.vedidev.nativebridge.cocos2dx;
+
+import com.vedidev.nativebridge.BunchManager;
+import com.vedidev.nativebridge.JsonSerializer;
+import com.vedidev.nativebridge.ProcessorEngine;
+import org.cocos2dx.lib.Cocos2dxActivity;
+import org.json.JSONObject;
+
+/**
+ * @author vedi
+ *         date 05/10/14
+ */
+public class Cocos2dxGatewayAdapter {
+
+    private static boolean contextSet = false;
+
+    @SuppressWarnings("UnusedDeclaration")
+    public static String dispatch(String strParams) {
+        if (!contextSet) {
+            BunchManager.getInstance().setContext(Cocos2dxActivity.getContext());
+            contextSet = true;
+        }
+
+        JSONObject dictParams = JsonSerializer.deserialize(strParams);
+        if (dictParams == null) {
+            return null;
+        }
+
+        JSONObject retDictParams = ProcessorEngine.getInstance().proceed(dictParams);
+
+        if (retDictParams == null) {
+            return null;
+        }
+
+        return JsonSerializer.serialize(retDictParams);
+    }
+}
